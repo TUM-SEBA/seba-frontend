e11756fe0928ca62bda6bca0960ebcdde962b404
rriyaldhi <rriyaldhi@gmail.com>
2020-06-09 20:49:45 +0200

    Connect Caretaker Page (Offer)

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,11 @@
 //URL
 export const serverURL = "http://localhost:5000";
 export const authURL = `${serverURL}/auth`;
+export const offerURL = `${serverURL}/offer`;
 
 //Messages
 export const userSignUpSuccessMsg = "User added successfully!!";
 export const passwordMismatch = "Passwords do not match!!";
 export const requiredFieldsEmpty = "Required fields are empty!!";
 export const loginSuccess = "Login Successful";
+export const fetchFailed = "Failed to fetch";

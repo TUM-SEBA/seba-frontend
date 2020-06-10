5942b992141430a3438658f6cf85426df0bfb358
Swornim Shrestha <ge82lor@mytum.de>
2020-06-10 03:35:41 +0200

    Initial commit for desigining of Welcome Page

---
+++ src/components/shared-component/Header.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import {AppBar, Toolbar, Typography, Grid} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+import {Avatar} from "@material-ui/core";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import ExitToAppOutlinedIcon from "@material-ui/icons/ExitToAppOutlined";
+import logo from "../../logoSEBA.png";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  medium: {
+    width: theme.spacing(5),
+    height: theme.spacing(5),
+  },
+  large: {
+    width: theme.spacing(10),
+    height: theme.spacing(10),
+  },
+
+  headerColor: {
+    background: "#DCEFDE",
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.headerColor}>
+      <AppBar className={classes.headerColor} position="static">
+        <Toolbar>
+          <Grid container direction="row" justify="space-between" alignItems="center">
+            <Grid item xs={8}>
+              <Avatar
+                className={classes.avatarStyle}
+                alt="Home"
+                src={logo}
+                className={classes.large}
+              />
+            </Grid>
+
+            <Button>
+              <AccountCircleIcon />
+              <Typography>Account Name</Typography>
+            </Button>
+
+            <Button>
+              <Typography>Sign Out</Typography>
+              <ExitToAppOutlinedIcon />
+            </Button>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import {Grid, Typography} from "@material-ui/core";
+import Header from "../components/shared-component/Header";
 
 export default function WelcomePage(props) {
-  return <div>Welcome</div>;
+  return (
+    <Grid container direction="column" justify="flex-start" alignItems="stretch">
+      <Grid item>
+        <Header />
+      </Grid>
+
+      <Grid item>
+        <Grid container direction="column" justify="flex-start" alignItems="stretch">
+          <Typography
+            style={{
+              margin: "auto",
+              fontSize: "25px",
+              width: "170px",
+              textAlign: "center",
+            }}
+          >
+            Hello User, XYZ Countinue as
+          </Typography>
+
+          <Grid container direction="row" justify="center" alignItems="center"></Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }

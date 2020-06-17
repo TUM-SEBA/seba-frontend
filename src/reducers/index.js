2f4d98e5ab301a32d2c63144513de3e55b651505
rriyaldhi <rriyaldhi@gmail.com>
2020-06-17 16:14:50 +0200

    Add Bidding Request List Dialog

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import {combineReducers} from "redux";
 import loginPage from "./loginPage";
 import caretakerPage from "./caretakerPage";
 import welcomePage from "./welcomePage";
+import ownerPage from "./ownerPage";
 
 const rootReducer = combineReducers({
   loginPage,
   caretakerPage,
   welcomePage,
+  ownerPage,
 });
 
 export default rootReducer;

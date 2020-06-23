bbc0d4499e5b8658aa630d375cbc95fb7cb222ac
Swornim Shrestha <ge82lor@mytum.de>
2020-06-24 00:59:48 +0200

    [MODIFIED] Removed orphan import (based on feedback of Rizky)

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -7,7 +7,6 @@ import ownerImage from "../assets/owner.png";
 import {makeStyles} from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import ViewBadges from "../components/ViewBadges";
-import {Link} from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   large: {

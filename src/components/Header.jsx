ce1029a82c8eb1f992f1f49c0a5f6d83cce67739
Swornim Shrestha <ge82lor@mytum.de>
2020-06-30 11:09:31 +0200

    [MODIFIED] Added switch user Button on Header. (TODO: Implement its functionality)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,7 @@ import Button from "@material-ui/core/Button";
 import {logout} from "../services/loginService";
 import AccountMenu from "./AccountMenu";
 import {connect} from "react-redux";
+import LoopIcon from "@material-ui/icons/Loop";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -43,13 +44,19 @@ const Header = (props) => {
       <AppBar className={classes.headerColor} position="static">
         <Toolbar>
           <Grid container direction="row" justify="space-around" alignItems="center">
-            <Grid item xs={4} md={8} sm={8} lg={10}>
+            <Grid item xs={3} md={8} sm={8} lg={9}>
               <Avatar alt="Home" src={logo} className={classes.large} />
             </Grid>
-            <Grid item xs={4} md={2} sm={2} lg={1}>
+            <Grid item xs={3} md={2} sm={2} lg={1}>
+              <Button>
+                <Typography className={classes.sentenceCase}>Switch User</Typography>
+                <LoopIcon />
+              </Button>
+            </Grid>
+            <Grid item xs={3} md={2} sm={2} lg={1}>
               <AccountMenu />
             </Grid>
-            <Grid item xs={4} md={2} sm={2} lg={1}>
+            <Grid item xs={3} md={2} sm={2} lg={1}>
               <Button onClick={logout}>
                 <Typography className={classes.sentenceCase}>Sign Out</Typography>
                 <ExitToAppOutlinedIcon />

ddf4112cd2319b071bdfe0e7dde9b93ec311d259
Eray Tufan <eray.tufan@siemens.com>
2020-07-09 17:00:08 +0200

    created footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import {AppBar, Toolbar, Typography, Grid, Container, Button} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+import {connect} from "react-redux";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import AddIcon from "@material-ui/core/SvgIcon/SvgIcon";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  footerColor: {
+    background: "darkgray",
+  },
+  footerText: {
+    margin: "auto",
+    width: "170px",
+    fontSize: "12px",
+  },
+  footerToolbar: {},
+  gridContainer: {
+    direction: "column",
+    justify: "center",
+    alignItems: "center",
+    spacing: "1",
+  },
+  socialMediaButton: {
+    backgroundColor: "transparent",
+  },
+}));
+
+const Footer = (props) => {
+  const classes = useStyles();
+
+  function openFacebook() {
+    window.open("https://www.facebook.com/TU.Muenchen");
+  }
+  function openInstagram() {
+    window.open("https://www.instagram.com/tu.muenchen/");
+  }
+  function openTwitter() {
+    window.open("https://twitter.com/TU_Muenchen");
+  }
+
+  return (
+    <div className={classes.footerColor}>
+      <AppBar className={classes.footerColor} position="static">
+        <Toolbar className={classes.footerToolbar}>
+          <Grid container className={classes.gridContainer}>
+            <Grid item xs={4}>
+              <Typography className={classes.footerText}>
+                Copyright @2020 Technical University of Munich
+              </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Grid container className={classes.gridContainer}>
+                <Grid item xs={4}>
+                  <Button
+                    variant="contained"
+                    className={classes.socialMediaButton}
+                    onClick={openFacebook}
+                  >
+                    <FacebookIcon fontSize="large" />
+                  </Button>
+                </Grid>
+                <Grid item xs={4}>
+                  <Button
+                    variant="contained"
+                    className={classes.socialMediaButton}
+                    onClick={openTwitter}
+                  >
+                    <TwitterIcon fontSize="large" />
+                  </Button>
+                </Grid>
+                <Grid item xs={4}>
+                  <Button
+                    variant="contained"
+                    className={classes.socialMediaButton}
+                    onClick={openInstagram}
+                  >
+                    <InstagramIcon fontSize="large" />
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={4}>
+              <Button variant="contained" className={classes.socialMediaButton}>
+                About Us
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Footer);

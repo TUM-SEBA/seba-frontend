9a3beac197864e944c4dec2b17a62ffab752b5a1
Eray Tufan <eray.tufan@siemens.com>
2020-07-13 16:41:58 +0200

    changed the title in review component

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -106,7 +106,7 @@ function Review(props) {
   return (
     <Dialog open={isReviewDialogOpen} onClose={() => setIsReviewDialogOpen(false, "")}>
       <DialogTitle id="show-reviews" disableTypography={true}>
-        <Typography variant="h5">Feedbacks</Typography>
+        <Typography variant="h5">Reviews</Typography>
       </DialogTitle>
       <DialogContent>
         <div className={classes.displayUs}>

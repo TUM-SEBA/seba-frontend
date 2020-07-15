f34c846a11391f71b40f7023407b4b49599b5345
Swornim Shrestha <ge82lor@mytum.de>
2020-07-15 21:15:26 +0200

    [MODIFIED] Changed Button Color of Bidding Request

--- src/components/BiddingRequestCard.jsx
+++ src/components/BiddingRequestCard.jsx
@@ -30,7 +30,8 @@ const styles = (theme) => {
     },
     reviewButton: {
       width: "100%",
-      backgroundColor: "darkgrey",
+      backgroundColor: "#307D3D",
+      color: "white",
     },
     buttonContainer: {
       margin: `${theme.spacing(1)}px 0`,
